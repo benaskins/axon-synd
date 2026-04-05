e50f45bdf7c41ff89f026ef03c1821c4ae450f40
Ben Askins <human@getlamina.ai>
2026-04-05 15:18:35 +1000

    docs: update migration comment to reference axon-base

--- migrations.go
+++ migrations.go
@@ -3,7 +3,7 @@ package synd
 import "embed"
 
 // Migrations contains the SQL migration files for axon-synd.
-// Composition roots pass this to axon.RunMigrations or axon.MustRunMigrations.
+// Composition roots pass this to migration.Run from axon-base.
 //
 //go:embed migrations/*.sql
 var Migrations embed.FS
